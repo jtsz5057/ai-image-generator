fa8b2d4aaf7e30614b26aa2e66394a117479f585
Jonathan Tan <jtsz5057@gmail.com>
2024-02-08 23:23:04 +1000

    connect to open ai api

--- server/routes/dalleRoutes.js
+++ server/routes/dalleRoutes.js
@@ -1,7 +1,18 @@
 import express from 'express';
 import * as dotenv from 'dotenv';
-import { Configuration, OpenAIApi } from 'openai';
+import OpenAI from 'openai';
 
 dotenv.config();
 
 const router = express.Router();
+
+const openai = new OpenAI({
+    apiKey: process.env.OPENAI_API_KEY,
+})
+
+router.route('/').get((req, res) => {
+    res.send('Hello from DALL-E!')
+})
+
+
+export default router;
